f80fef8a1358f37322249850353fa8af8b4dd16f
pkeerasuntonpong <pkeerasuntonpong@berklee.edu>
2017-04-24 10:45:53 -0400

    Restart Button
    Add restart button to scene.

---
+++ Assets/Scripts/RestartButton.cs
@@ -0,0 +1,26 @@
+﻿//LMSC-281
+//Prin Keerasuntonpong
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class RestartButton : MonoBehaviour
+{
+    //Get button and stores it int yourButton
+    public Button yourButton;
+
+    void Start()
+    {
+        //Get Button component and stores it in btn
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        //Reload scene
+        SceneManager.LoadScene("RollerBall");
+    }
+}
