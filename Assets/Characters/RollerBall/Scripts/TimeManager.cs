228eb00b69f8ce79fd37bf77cdd29b81458ae712
kjhar474 <kenneth.harmon62@gmail.com>
2017-04-24 17:30:19 -0400

    Timer Functionality
    Added a UI text element to display timer and a script to use it.

---
+++ Assets/Characters/RollerBall/Scripts/TimeManager.cs
@@ -0,0 +1,39 @@
+﻿// Kenny Harmon
+// LMSC-281
+// Roller Ball Edit Assignment
+// Timer Mgmt Script
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour 
+{
+
+	public static float timer; 						// float because Time.deltaTime is a float
+
+	Text text;
+
+	void Awake()
+	{
+		text = GetComponent<Text>();
+		timer = 20;
+	}
+
+	void Update()
+	{
+		timer -= Time.deltaTime; 					// subtract seconds of runtime from counter
+		setTimer(Mathf.RoundToInt(timer));			// round the float timer result and display
+
+		if (timer <= 0)								// disallow negative time
+		{
+			setTimer(0);							// change later to trigger game end sequence
+		}
+	}
+
+	void setTimer(int time)	
+	{
+		text.text = "Timer: " + time;
+	}
+}
