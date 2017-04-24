4c43e52983a3f500779805c322ae9ccb24637baf
Augustus Rivera <azrivera@berklee.edu>
2017-04-24 17:58:31 -0400

    Added timer functionality, a red goal post that ends the restarts the game when touched, and music.

---
+++ Assets/Characters/RollerBall/Scripts/GameOverManager.cs
@@ -0,0 +1,32 @@
+﻿//LMSC 281
+//Augustus Rivera
+//Roller Ball Mods
+//Game Over Script
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
+
+
---
+++ Assets/Characters/RollerBall/Scripts/TimeManager.cs
@@ -0,0 +1,41 @@
+﻿//LMSC 281
+//Augustus Rivera
+//Roller Ball Mos
+//Time Manager Script
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeManager : MonoBehaviour {
+
+	public static float timer;
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent <Text> ();
+		timer = 5f;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		timer -= Time.deltaTime;
+		text.text = "Timer: " + Mathf.RoundToInt(timer);
+
+
+	}
+
+	void RestartCurrentScene()
+	{
+		if (Time.deltaTime >= 5f) 
+		{
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+}
+
