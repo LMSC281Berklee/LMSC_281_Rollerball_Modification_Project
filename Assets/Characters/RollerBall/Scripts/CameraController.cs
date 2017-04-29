1ebcd042508081ddf53ab16b25f3e4ff15749e90
Maeve Higgins <maevehiggins123@gmail.com>
2017-04-29 17:45:36 -0400

    Added timer!

--- Assets/Characters/RollerBall/Scripts/CameraController.cs
+++ Assets/Characters/RollerBall/Scripts/CameraController.cs
@@ -3,20 +3,32 @@
 
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
+	public string timer; 
+	public float timedown;
+	public Text counttext;
 
 	private Vector3 offset;
 
 	void Start ()
 	{
+
 		offset = transform.position - player.transform.position;
 	}
 
 	void LateUpdate ()
 	{
+		counttext.text = timer;
+		timer = timedown.ToString();
 		transform.position = player.transform.position + offset;
+		timedown = 30 - Time.fixedTime;
+		if (timedown <= 0) 
+		{player.gameObject.SetActive(false);
+			timer="GAMEOVER";}
 	}
 }
