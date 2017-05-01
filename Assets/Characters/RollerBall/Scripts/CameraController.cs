bba9baef5ac6e8235ac0d8d6fd858744505f7b60
djeong1 <glqkfl2107@da-eun-ui-MacBook-Pro.local>
2017-05-01 14:50:22 -0400

    changed camera angle, add jump function

--- Assets/Characters/RollerBall/Scripts/CameraController.cs
+++ Assets/Characters/RollerBall/Scripts/CameraController.cs
@@ -1,22 +1,32 @@
-﻿//Rollerball Unity Tutorial Modification
+//Rollerball Unity Tutorial Modification
 //LMSC-281 Spring 2017
+//referred Unity Forum
 
 using UnityEngine;
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject player;
+	public float turnSpeed = 5.0f;
+	public Transform player;
 
-	private Vector3 offset;
+	public float height = 2f;
+	public float distance = 3f;
+
+	private Vector3 offsetX;
+	private Vector3 offsetY;
 
 	void Start ()
 	{
-		offset = transform.position - player.transform.position;
+		offsetX = new Vector3 (0, height, distance);
+		offsetY = new Vector3 (0, 0, distance);
 	}
 
 	void LateUpdate ()
 	{
-		transform.position = player.transform.position + offset;
+		offsetX = Quaternion.AngleAxis (Input.GetAxis ("Mouse X") * turnSpeed, Vector3.up) * offsetX;
+		offsetY = Quaternion.AngleAxis (Input.GetAxis ("Mouse Y") * turnSpeed, Vector3.right) * offsetY;
+		transform.position = player.position + offsetX;
+		transform.LookAt (player.position);
 	}
 }
--- Assets/Characters/RollerBall/Scripts/PlayerMovement.cs
+++ Assets/Characters/RollerBall/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-﻿//Rollerball Unity Tutorial Modification
+//Rollerball Unity Tutorial Modification
 //LMSC-281 Spring 2017
 
 using UnityEngine;
@@ -8,12 +8,21 @@ public class PlayerMovement : MonoBehaviour {
 
 
 	public float speed;
+	public Vector3 jump;
+	public float jumpForce = 2.0f;
 
+	public bool onGround;
 	private Rigidbody rb;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		jump = new Vector3 (0, 2f, 0);
+	}
+
+	void OnCollisionStay()
+	{
+		onGround = true;
 	}
 
 	void FixedUpdate ()
@@ -25,5 +34,11 @@ public class PlayerMovement : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+
+		if (Input.GetKeyDown (KeyCode.Space) && onGround)
+		{
+			rb.AddForce (jump * jumpForce, ForceMode.Impulse);
+			onGround = false;
+		}
 	}
 }
